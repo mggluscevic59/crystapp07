b2c244c545a5493564396ae54bd6f3cda04b7c16
Mišo Grgur Gluščević <mggluscevic59@gmail.com>
2023-03-26 15:41:14 +0200

    Grab a temp from mock
    Implemented first test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,5 +1,22 @@
 import unittest
+import julabo
 
-class integrationTester(unittest.BaseTestSuite):
-    def test_mock_connection(self):
-        pass
+from crystapp.mocks import JulaboMock
+
+class integrationTester(unittest.TestCase):
+    def test_mock_temp(self):
+        # Arrange
+        mock = JulaboMock(".venv",".fixture.yml")
+        mock.start()
+        fixture = {
+            "url"               : "tcp://localhost:5050",
+            "concurrency"       : "syncio",
+            "auto_reconnect"    : True
+        }
+        sut = julabo.JulaboCF(julabo.connection_for_url(**fixture))
+        
+        # Act
+        result = sut.external_temperature()
+        
+        # Assert
+        self.assertEqual("28.22", result)
