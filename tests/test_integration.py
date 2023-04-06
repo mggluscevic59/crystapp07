54dfd0a7e4b114b625a328bc8d262bc71244747e
Mišo Grgur Gluščević <mggluscevic59@gmail.com>
2023-04-06 11:22:14 +0200

    Refactor two working tests
    Should write more of them...

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -4,50 +4,45 @@ import crystapp
 
 class integrationTester(unittest.TestCase):
     def setUp(self) -> None:
+        self.fixtures = {
+            "environment"   : ".venv",
+            "configuration" : ".fixture.yml",
+            "url"           : "tcp://localhost:5050",
+            "baseObject"    : ".config/server.xml",
+        }
         self._env_path = ".venv"
         self._mock_path = ".fixture.yml"
         self._mock_url = "tcp://localhost:5050"
-
-        # virtual device server for everybody
-        self.fixture = crystapp.Mock(self._env_path,self._mock_path)
-        # self.fixture = crystapp.julabo.mock.Mock(self._env_path,self._mock_path)
-        self.fixture.start()
         return super().setUp()
-
-    def tearDown(self) -> None:
-        self.fixture.stop()
-        return super().tearDown()
 
     def test_mock_temp_min(self):
         # Arrange
-        # FIXME: debug mock running from root folder -> transit to direct call
-        result = 0
-        fixture = {
-            "url"               : self._mock_url,
-            "concurrency"       : "syncio",
-            "auto_reconnect"    : True
-        }
-        sut = julabo.JulaboCF(julabo.connection_for_url(**fixture))
-        # TODO: act with internal mock/device connection -> wrapper
+        sut = crystapp.Driver(self.fixtures["url"])
+        # NOTE: integration testing, socket involved
+        # NOTE: should test all decision branches
+        # BUG: individual tests should be independent (:5050 -> :0)
 
         # Act
-        result = sut.external_temperature()
+        with crystapp.Mock(self.fixtures["environment"], self.fixtures["configuration"]):
+            result = sut.methods["external_temperature"]()
 
         # Assert
         self.assertEqual(28.22, result)
 
     def test_server_temp_min(self):
         # Arrange
-        result = None
-        with crystapp.Server([self._mock_url]) as sut:
-            node_list = sut.import_xml_and_populate_devices(".config/server.xml")
-            device_idx = sut.get_namespace_index(self._mock_url)
-            # TODO: optimise => no hidden server access; resistance to refactoring
-            object_type_idx, name = crystapp.find_object_type(node_list, sut._server)
-            device = sut.objects.get_child(f"{device_idx}:{name}")
+        sut = crystapp.Server([self.fixtures["url"]])
+        node_list = sut.import_xml_and_populate_devices(self.fixtures["baseObject"])
+        device_idx = sut.get_namespace_index(self.fixtures["url"])
+        object_type_idx, name = crystapp.find_object_type(node_list, sut._server)
+        device = sut.objects.get_child(f"{device_idx}:{name}")
+        # TODO: optimise => no hidden server access; resistance to refactoring
 
-            # Act
-            result = device.call_method(f"{object_type_idx}:External_temperature", 0)
+        # Act
+        with crystapp.Mock(self.fixtures["environment"], self.fixtures["configuration"]):
+            with sut:
+                result = device.call_method(f"{object_type_idx}:External_temperature", 0)
+
         # Assert
         self.assertEqual(28.22, result)
 
@@ -61,6 +56,3 @@ class integrationTester(unittest.TestCase):
     #         # Assert
     #         finally:
     #             self.fixture.start()
-
-    # def test_semi_server(self):
-    #     pass
